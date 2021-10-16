4810dac433d92e1596385f0979fca58bfb6ea9b3
Konrad570 <Konradtoesch@icloud.com>
2021-10-16 16:28:07 +0200

    Init

---
+++ README.md
@@ -0,0 +1,10 @@
+# Server
+Basic HTTP Authentification (Name, Password) to protect a page.
+
+# Information
+- Framework: vibe.d | Language: D-lang
+
+# Quick Start
+- Download, make sure D is installed.
+- Run with: dub
+- Enter https://localhost:8000
---
+++ source/app.d
@@ -0,0 +1,31 @@
+module app; 
+import vibe.vibe; 
+
+import vibe.appmain;
+import vibe.http.auth.basic_auth;
+import vibe.http.router;
+import vibe.http.server;
+import std.functional : toDelegate;
+
+bool checkPassword(string user, string password) 
+{
+	return user == "admin" && password == "password";
+}
+
+void main() {
+	auto router = new URLRouter; 
+
+	router.get("/", staticTemplate!"index.dt"); 
+
+	router.any("*", performBasicAuth("Site Realm", toDelegate(&checkPassword))); 
+
+	router.get("/admin", staticTemplate!"admin.dt");
+
+	auto settings = new HTTPServerSettings; 
+	settings.port = 8000;
+	settings.bindAddresses = ["::1", "127.0.0.1"]; 
+
+	auto listener = listenHTTP(settings, router); 
+	scope(exit) listener.stopListening(); 
+	runApplication();
+}
